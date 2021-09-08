4ddb7f083c9d9864baf6887577b6ba3b099f9aad
Matheus Dias <matheus.sanches@saedigital.com.br>
2021-09-08 00:07:32 -0300

    Typescript integration

---
+++ example-ts/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import ExampleProject from './prestion/project';
+
+function App() {
+
+  useEffect(() => {
+    const pr = document.getElementsByClassName('prestion')[0]
+
+
+    // @ts-ignore
+    for (let child of pr.children) {
+      child.remove()
+    }
+    
+    document.querySelectorAll('canvas, .prestion_tools').forEach(e => e.remove())
+    
+    const prestion = new ExampleProject()
+    
+    // @ts-ignore
+    window.prestion = prestion
+    
+    prestion.load()
+      .then(() => prestion.start())
+
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="prestion" />
+    </div>
+  );
+}
+
+export default App;
